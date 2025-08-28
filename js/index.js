1d058ff96c4cb6d50148a8f412864b4344f930a5
shihab-63 <thecodzhub@gmail.com>
2025-08-28 17:15:05 +0600

    Add JavaScript for callig button and coun counter

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ function getElement(id) {
     return element;
 }
 
-// Heart Icons Functionality
+// Heart Icons javaScript Functionality
 document.getElementById('card-container').addEventListener('click', function(e){
     if (e.target.className.includes('love-icon')) {
         if (e.target.className.includes('text-red-500') === false) {
@@ -21,4 +21,30 @@ document.getElementById('card-container').addEventListener('click', function(e){
             e.target.classList.remove('fa-solid');
         }    
     }
+
+    // All Files
+
+    const callButton = e.target.closest('.call-btn')
+    if (callButton) {
+        const serviceName = callButton.parentNode.parentNode.children[1].children[1].innerText;
+        const serviceNumber = callButton.parentNode.parentNode.children[2].children[0].innerText;
+        
+
+        // Coin Counter
+        const coinCounter = Number(getElement('coin-counter').innerText);
+
+        if(coinCounter >= 20){
+            getElement('coin-counter').innerText = coinCounter - 20;
+        }else {
+            alert("Sorry, you don't have enough coins to make a call.")
+            return;
+        }
+        alert(`📞 Calling ${serviceName} Service ${serviceNumber}...`)
+    }
 });
+
+
+// Call Buttons javaScript Functionality
+// document.getElementById('card-container').addEventListener('click', function(e){
+    
+// })
